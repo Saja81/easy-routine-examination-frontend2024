de92e386bb777ce1a049aa1827eb9b5bce160d78
Sandr <sandrajakobsson81@gmail.com>
2024-05-13 14:40:10 +0200

    Testen to update code to try auto deploying on Netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ const IndexPage = () => (
     <h1>Easy Routine</h1>
 
     <p>Välkommen till Easy Routine - Appen som hjälper dig planera vardagen</p>
+    <p>Recept</p>
+    <p>Träning</p>
+    <p>Schema</p>
   </>
 )
 export default IndexPage
