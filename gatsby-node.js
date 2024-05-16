66f323d4bd8f8890eebdf18f5093a4fa99481095
Sandr <sandrajakobsson81@gmail.com>
2024-05-16 13:11:16 +0200

    Developed a dynamic weekly menu page with api calls to contentul

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,200 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
+const path = require("path")
 
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
-exports.createPages = async ({ actions }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+
+  // Define the template for individual projects
+  // const projectTemplate = path.resolve("./src/templates/ProjectPosts.js")
+
+  // Define the template for individual menyv1 posts
+  // const menyTemplateV1 = path.resolve("./src/templates/MenyPostsv1.js")
+
+  // Define the template for individual menyv2 posts
+  // const menyTemplateV2 = path.resolve("./src/templates/MenyPostsv2.js")
+
+  // // Define the template for individual menyv2 posts
+  // const menyTemplateV3 = path.resolve("./src/templates/MenyPostsv3.js")
+
+  // // Define the template for individual menyv2 posts
+  // const menyTemplateV4 = path.resolve("./src/templates/MenyPostsv4.js")
+
+  // Fetch data from Contentful for projects
+  const projectsResult = await graphql(`
+    {
+      allContentfulProjekt {
+        nodes {
+          titel
+          slug
+          kategori
+        }
+      }
+    }
+  `)
+
+  if (projectsResult.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful projects`,
+      projectsResult.errors
+    )
+    return
+  }
+
+  const projects = projectsResult.data.allContentfulProjekt.nodes
+
+  if (projects.length > 0) {
+    projects.forEach(project => {
+      createPage({
+        path: `/${project.slug}/`,
+        component: projectTemplate,
+        context: {
+          slug: project.slug,
+        },
+      })
+
+      createPage({
+        path: `/${project.kategori.toLowerCase()}/`, // Lägg till kategori i sökvägen
+        component: projectTemplate,
+        context: {
+          slug: project.slug,
+          kategori: project.kategori, // Skicka med kategorin som en del av context
+        },
+      })
+    })
+  }
+
+  // Fetch data from Contentful for menyv1 posts
+  const menyResultV1 = await graphql(`
+    {
+      allContentfulMenyV1 {
+        nodes {
+          dag
+          slug
+        }
+      }
+    }
+  `)
+
+  if (menyResultV1.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful menyv1 posts`,
+      menyResultV1.errors
+    )
+    return
+  }
+
+  const menyPostsV1 = menyResultV1.data.allContentfulMenyV1.nodes
+
+  if (menyPostsV1.length > 0) {
+    menyPostsV1.forEach(post => {
+      createPage({
+        path: `/menyv1/${post.slug}/`,
+        component: menyTemplateV1,
+        context: {
+          slug: post.slug,
+        },
+      })
+    })
+  }
+
+  // Fetch data from Contentful for menyv2 posts
+  const menyResultV2 = await graphql(`
+    {
+      allContentfulMenyV2 {
+        nodes {
+          dag
+          slug
+        }
+      }
+    }
+  `)
+
+  if (menyResultV2.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful menyv2 posts`,
+      menyResultV2.errors
+    )
+    return
+  }
+
+  const menyPostsV2 = menyResultV2.data.allContentfulMenyV2.nodes
+
+  if (menyPostsV2.length > 0) {
+    menyPostsV2.forEach(post => {
+      createPage({
+        path: `/menyv2/${post.slug}/`,
+        component: menyTemplateV2,
+        context: {
+          slug: post.slug,
+        },
+      })
+    })
+  }
+
+  // Fetch data from Contentful for menyv3 posts
+  const menyResultV3 = await graphql(`
+    {
+      allContentfulMenyV3 {
+        nodes {
+          dag
+          slug
+        }
+      }
+    }
+  `)
+
+  if (menyResultV3.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful menyv3 posts`,
+      menyResultV3.errors
+    )
+    return
+  }
+
+  const menyPostsV3 = menyResultV3.data.allContentfulMenyV3.nodes
+
+  if (menyPostsV3.length > 0) {
+    menyPostsV3.forEach(post => {
+      createPage({
+        path: `/menyv3/${post.slug}/`,
+        component: menyTemplateV3,
+        context: {
+          slug: post.slug,
+        },
+      })
+    })
+  }
+
+  // Fetch data from Contentful for menyv3 posts
+  const menyResultV4 = await graphql(`
+    {
+      allContentfulMenyV4 {
+        nodes {
+          dag
+          slug
+        }
+      }
+    }
+  `)
+
+  if (menyResultV4.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful menyv4 posts`,
+      menyResultV4.errors
+    )
+    return
+  }
+
+  const menyPostsV4 = menyResultV4.data.allContentfulMenyV4.nodes
+
+  if (menyPostsV4.length > 0) {
+    menyPostsV4.forEach(post => {
+      createPage({
+        path: `/menyv4/${post.slug}/`,
+        component: menyTemplateV4,
+        context: {
+          slug: post.slug,
+        },
+      })
+    })
+  }
 }
--- src/pages/RecepiesApp.js
+++ src/pages/RecepiesApp.js
@@ -2,103 +2,99 @@ import * as React from "react"
 import CardComponentRecepies from "../components/CardComponentRecepies"
 import { Box, Card, Container, Stack, Typography } from "@mui/material"
 import Image from "mui-image"
-// Read more about Image from mui: https://www.npmjs.com/package/mui-image
 import Layout from "../components/Layout"
-import food from "./food.jpg"
+import { graphql } from "gatsby"
 
-const RecepiesApp = () => (
-  <Layout>
-    <Container>
-      <Card
-        variant="none"
-        sx={{
-          my: 2,
-        }}
-      >
-        <Stack alignItems="center" my={2}>
-          <Typography variant="h5" mb={4}>
-            Meny vecka 20
-          </Typography>
-          <Stack direction="column" gap={1}>
-            <Stack direction={{ xs: "column", md: "row" }} gap={1}>
+const RecepiesApp = ({ data }) => {
+  // Function to sort weekdays.
+  const sortingFunction = (a, b) => {
+    // Weekdays in Swedish
+    const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+    // Get the weekday from a and b
+    const dayA = a.node.day
+    const dayB = b.node.day
+
+    // Get the index for a and b in the weekdays array
+    const indexA = weekdays.indexOf(dayA)
+    const indexB = weekdays.indexOf(dayB)
+
+    // Compare indexes to sort
+    if (indexA < indexB) {
+      return -1
+    }
+    if (indexA > indexB) {
+      return 1
+    }
+    return 0
+  }
+
+  const sortedPosts = data.allContentfulMenyV1.edges.sort(sortingFunction)
+
+  return (
+    <Layout>
+      <Container>
+        <Card
+          variant="none"
+          sx={{
+            my: 2,
+          }}
+        >
+          <Stack alignItems="center" my={2}>
+            <Typography variant="h5" mb={4}>
+              Meny vecka 20
+            </Typography>
+            {sortedPosts.map(edge => (
               <Stack
-                alignItems="center"
-                border="solid 1px silver"
-                p={1}
-                gap={1}
-                width="200px"
+                key={edge.node.dag}
+                direction={{ xs: "column", md: "row" }}
+                gap={4}
               >
-                <Typography variant="h6">Måndag</Typography>
-                <Typography variant="h8">Köttbullar med mos</Typography>
-                <Image src={food} width={100} />
+                <Stack
+                  alignItems="center"
+                  border="solid 1px silver"
+                  p={1}
+                  gap={1}
+                  width="200px"
+                >
+                  <Typography variant="h6">{edge.node.dag}</Typography>
+                  <Typography variant="h8">{edge.node.rtt}</Typography>
+                  {/* Rendera andra värden här */}
+
+                  <Image src={edge.node.bild.file.url} width={100} />
+                </Stack>
               </Stack>
-              <Stack
-                alignItems="center"
-                border="solid 1px silver"
-                p={1}
-                gap={1}
-                width="200px"
-              >
-                <Typography variant="h6">Tisdag</Typography>
-                <Typography variant="h8">
-                  Snitzel med råstekt potatis
-                </Typography>
-                <Image src={food} width={100} />
-              </Stack>
-              <Stack
-                alignItems="center"
-                border="solid 1px silver"
-                p={1}
-                gap={1}
-                width="200px"
-              >
-                <Typography variant="h6">Onsdag</Typography>
-                <Typography variant="h8">Kycklingwok med ris</Typography>
-                <Image src={food} width={100} />
-              </Stack>
-            </Stack>
-            <Stack direction={{ xs: "column", md: "row" }} gap={1}>
-              <Stack
-                alignItems="center"
-                border="solid 1px silver"
-                p={1}
-                gap={1}
-                width="200px"
-              >
-                <Typography variant="h6">Torsdag</Typography>
-                <Typography variant="h8">Korvstroganoff med mos</Typography>
-                <Image src={food} width={100} />
-              </Stack>
-              <Stack
-                alignItems="center"
-                border="solid 1px silver"
-                p={1}
-                gap={1}
-                width="200px"
-              >
-                <Typography variant="h6">Fredag</Typography>
-                <Typography variant="h8">
-                  Fläskifilegryta med potatis
-                </Typography>
-                <Image src={food} width={100} />
-              </Stack>
-              <Stack
-                alignItems="center"
-                border="solid 1px silver"
-                p={1}
-                gap={1}
-                width="200px"
-              >
-                <Typography variant="h6">Inköp</Typography>
-                <Typography variant="h8">Potatis</Typography>
-              </Stack>
-            </Stack>
+            ))}
           </Stack>
-        </Stack>
-      </Card>
-    </Container>
-    {/* <CardComponentRecepies /> */}
-  </Layout>
-)
+        </Card>
+      </Container>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query MyQuery {
+    allContentfulMenyV1 {
+      edges {
+        node {
+          rtt
+          dag
+          bild {
+            file {
+              url
+            }
+          }
+          ingredienser {
+            raw
+          }
+          lnk
+          grSHr {
+            raw
+          }
+        }
+      }
+    }
+  }
+`
 
 export default RecepiesApp
