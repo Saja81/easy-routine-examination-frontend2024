e85dd677562ce4b29f0df93321d1ce670e23d732
Sandr <sandrajakobsson81@gmail.com>
2024-05-27 12:39:53 +0200

    Added env for produvtion

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin")
-const serviceAccount = require(`.env.${process.env.GATSBY_GOOGLE_APPLICATION_CREDENTIALS}`)
+const serviceAccount = `.env.${process.env.GATSBY_GOOGLE_APPLICATION_CREDENTIALS}`
 // const serviceAccount = require("./keys/serviceAccountKey.json")
 
 admin.initializeApp({
