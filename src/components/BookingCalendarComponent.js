409c8032d9a13e02010c706688ac892c4c6b83f5
Sandr <sandrajakobsson81@gmail.com>
2024-06-02 11:05:37 +0200

    Small changes design bookingapp

--- src/components/BookingCalendarComponent.js
+++ src/components/BookingCalendarComponent.js
@@ -209,7 +209,17 @@ const CalendarComponent = () => {
         </>
       )}
 
-      <Dialog open={viewBookings} onClose={handleToggleBookings}>
+      <Dialog
+        open={viewBookings}
+        onClose={handleToggleBookings}
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: "auto",
+        }}
+      >
         <Box m={6}>
           <DialogTitle>
             Bokningar
