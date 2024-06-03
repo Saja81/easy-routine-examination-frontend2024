98941278e6dc5041a4850d52eb248699eda0d3ca
Sandr <sandrajakobsson81@gmail.com>
2024-06-03 10:15:43 +0200

    .

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+// import { initializeApp } from "firebase/app"
+// import { getFirestore } from "firebase/firestore"
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyB7JwO6IKYWX_TWha2H5pGK9mY2r_yOdXA",
-  authDomain: "easy-routine.firebaseapp.com",
-  projectId: "easy-routine",
-  storageBucket: "easy-routine.appspot.com",
-  messagingSenderId: "946342461835",
-  appId: "1:946342461835:web:bb118233ffe29fe2912f8e",
-  measurementId: "G-M0DDJV7YHV",
-}
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyB7JwO6IKYWX_TWha2H5pGK9mY2r_yOdXA",
+//   authDomain: "easy-routine.firebaseapp.com",
+//   projectId: "easy-routine",
+//   storageBucket: "easy-routine.appspot.com",
+//   messagingSenderId: "946342461835",
+//   appId: "1:946342461835:web:bb118233ffe29fe2912f8e",
+//   measurementId: "G-M0DDJV7YHV",
+// }
 
-export const firebaseApp = initializeApp(firebaseConfig)
-export const db = getFirestore(firebaseApp)
+// export const firebaseApp = initializeApp(firebaseConfig)
+// export const db = getFirestore(firebaseApp)
